8bed8220ae923ce0cada69c07e232e8f67929766
FoxyTony <mirantony@icloud.com>
2021-02-09 10:19:04 +0100

    Wrote first test passing

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
-
+# CBlSgBCHgn94ZnNDC0coGBDna5txAZqK2
 ruby '3.0.0'
 gem 'rails', '~> 6.1.1'
 gem 'pg', '~> 1.1'
---
+++ spec/requests/api/api_responds_with_a_collection_of_articles_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe 'GET /api/articles', types: :request  do
+  describe 'successfully' do
+    before do
+      get '/api/articles'
+    end
+
+
+      it 'is expected to return a 200 response status' do
+        expect(response).to have_http_status 200
+      end
+
+      it 'is expected to return all articles' do
+        expect(response_json['articles'].count).to eq 3
+      end
+
+      it 'is expected to return articles titles' do
+        expect(response_json['articles'].first['title']).to eq 'This is an awesome title'
+      end
+    end
+  
+end
